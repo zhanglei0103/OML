f1ce23714ebe758e6b27f3c493a74fa76007fc98
zhanglei0103 <zxy1419@mail.ustc.edu.cn>
2024-01-10 09:51:42 +0800

    Delete utils directory

--- utils/utils_other_helps.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import pandas as pd
-import streamlit as st
-
-@st.cache_data
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
